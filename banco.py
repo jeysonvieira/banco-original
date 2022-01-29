2bdc08a284c6c55dbb1b8bc5ad55cead8215fb21
jeyson saraiva <jeysonsaraiva@gmail.com>
2022-01-29 15:24:11 -0300

    sistema com banco de dados

--- banco.py
+++ banco.py
@@ -2,11 +2,12 @@
 #from concurrent.futures.process import _SafeQueue
 #from tkinter.filedialog import SaveFileDialog
 #from typing_extensions import Self
+from random import randint
 
 
 print('=-'*15,'BEM VINDO AO BANCO JEYSONCARD.', '=-'*15)
 
-print('=-'*15,'Preencha seus dados corretamente para prosseguir''=-'*15)
+print('=-'*15,'Preencha seus dados corretamente para prosseguir','=-'*15)
 #Cliente
 class Cliente():
     def __init__(self, Nome, Cartão, Cpf, Senha, Saldo):
@@ -37,25 +38,67 @@ class Cliente():
     def saida(self):
         self.saida = 'obrigado, Volte sempre'
         return self.saida
+
+#começo
+resp = ' '
+while resp not in 'UN':
+    resp = str.upper(input('VOCÊ JÁ É USÚARIO DO BANCO OU É NOVATO E DESEJA FAZER UM CADASTRO?[U/N]: '))
+
+
+#Banco de Dados.
+lista = [['JEYSON SARAIVA VIEIRA', '086409511', '55385554960', '307119', 3200],
+ ['ISADORA SOUSA DE CARVALHO', '019331863', '49538001541', '182315', 1800],
+ ['EDUARDO RIBEIRO MOURA', '071151587', '38117715120', '540820', 2400]]
+
+
+novo_usuario = list()
 #Informações do Cliente
-nome = str(input('Digite seu nome completo: '))
-cartao = int(input('Digite o número do seu cartão: '))
-cpf = int(input('Digite o número do seu cpf: '))
-senha = int(input('Digite sua senha: '))
-saldo = float(input('Digite seu saldo: '))
+if resp == 'U':
+    nome = str.upper(input('Digite seu nome completo: '))
+    if nome == lista[0][0]:
+        posiçao = 0
+
+    elif nome == lista[1][0]:
+        posiçao = 1
+
+    elif nome == lista[2][0]:
+        posiçao = 2
+
+    if nome == lista[0][0] or lista[1][0] or lista[2][0]:
+        cartao = int(input('Digite o número do seu cartão: '))
+        cpf = int(input('Digite o número do seu cpf: '))
+        senha = int(input('Digite sua senha: '))
+        saldo = float(input('Digite seu saldo: '))
+        print(lista[posiçao])
+        cliente = Cliente(Nome = lista[posiçao][0], Cartão = lista[posiçao][1], Cpf = lista[posiçao][2], Senha = lista[posiçao][3], Saldo = lista[posiçao][4])
+
+
+    else:
+        print('SEU NOME NÃO FOI ENCONTRADO NO BANCO DE DADOS, CORRIGA SEU NOME OU FAÇA UM CADASTRO PARA VOCÊ.')
+
+if resp == 'N':
+    print('=-'*15, 'VAMOS FAZER UM CADASTRO PARA VOCÊ', '=-'*15)
+    novo_usuario.append(str.upper(input('Digite seu nome completo: ')))
+    novo_usuario.append(randint(111111111, 999999999))
+    novo_usuario.append(int(input('Digite seu cpf: ')))
+    novo_usuario.append(int(input('Crie uma senha de 6 digitos: ')))
+    novo_usuario.append(float(input('Digite seu saldo inicial: ')))
+    print(novo_usuario)
+    cliente = Cliente(Nome = novo_usuario[0], Cartão = novo_usuario[1], Cpf = novo_usuario[2], Senha = novo_usuario[3], Saldo = novo_usuario[4])
 
 
 
-cliente = Cliente(Nome = nome, Cartão = cartao, Cpf = cpf, Senha = senha, Saldo = saldo)
+#cliente_novo = Cliente(Nome = lista[3][0], Cartão = lista[3][1], Cpf = lista[3][2], Senha = lista[3][3], Saldo = lista[3][4])
 
 #Interação com a maquina
 print('[1] DEPOSITAR '
       '[2] SACAR '
       '[3] DADOS '
-      '[4] SAIR'
+      '[4] SALDO '
+      '[5] SAIR'
 )
 resp = ' '
-while resp not in '1234':
+while resp not in '12345':
     resp = str(input('Qual dessas opções deseja escolher?: '))
 if resp == '1':
     print(cliente.saldo)
@@ -71,6 +114,8 @@ elif resp == '3':
     print(cliente.dados())
 
 elif resp == '4':
-    print(cliente.saida())
+    print(cliente.saldo)
 
+elif resp == '5':
+    print(cliente.saida)
 #Fim
